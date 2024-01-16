8484233b01af72bbfc45cfa3d58d28367e768e30
kngocc <kngocc66@gmail.com>
2024-01-16 17:56:57 +0700

    starbuckk

--- js/menu.js
+++ js/menu.js
@@ -14,6 +14,8 @@ function getProductList() {
     });
 }
 
+
+
 function renderProductlist() {
   for (let index = 0; index < productList.length; index++) {
     const product = productList[index];
@@ -43,3 +45,152 @@ function navigateToDetail(type) {
 
 
 getProductList();
+
+
+
+
+
+let productLis = [];
+
+
+const productFoodElm = document.getElementById("product-lis");
+const productFood = document.getElementById("product-templat");
+
+function getProductLis() {
+  fetch("https://6578608df08799dc80451933.mockapi.io/products?parent_type=food")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      productLis = data;
+      renderProductlis();
+    });
+}
+function renderProductlis() {
+  for (let index = 0; index < productLis.length; index++) {
+    const produc = productLis[index];
+
+    const productIte = productFood.content.cloneNode(true);
+    const productImag = productIte.getElementById("product-imag")
+    productImag.src = produc.image;
+    productIte.getElementById("product-titl").textContent = produc.child_type;
+    productImag.addEventListener('click', function() {
+      console.log(1);
+      navigateToDetai(produc.child_type)
+    })
+    productFoodElm.appendChild(productIte);
+  }
+}
+
+function navigateToDetai(type) {
+  console.log(1);
+  window.location = `./detail.html?type=${type}`
+}
+
+
+getProductLis();
+
+
+
+
+let productList1 = [];
+
+const productListElm1 = document.getElementById("product-list1");
+const productTemp1 = document.getElementById("product-template1");
+
+function getProductList1() {
+  fetch("https://6578608df08799dc80451933.mockapi.io/products?parent_type=at+home+coffee")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      productList1 = data;
+      renderProductlist1();
+    });
+}
+
+
+
+function renderProductlist1() {
+  for (let index = 0; index < productList1.length; index++) {
+    const product1 = productList1[index];
+
+    const productItem1 = productTemp1.content.cloneNode(true);
+    const productImage1 = productItem1.getElementById("product-image1")
+    productImage1.src = product1.image;
+    productItem1.getElementById("product-title1").textContent = product1.child_type;
+    productImage1.addEventListener('click', function() {
+      console.log(1);
+      navigateToDetail1(product1.child_type)
+    })
+    productListElm1.appendChild(productItem1);
+  }
+}
+
+/////
+
+
+function navigateToDetail1(type) {
+  console.log(1);
+  window.location = `./detail.html?type=${type}`
+}
+
+
+
+
+
+getProductList1();
+
+
+
+let productList2 = [];
+
+const productListElm2 = document.getElementById("product-list2");
+const productTemp2 = document.getElementById("product-template2");
+
+function getProductList2() {
+  fetch("https://6578608df08799dc80451933.mockapi.io/products?parent_type=merchandise")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      productList2 = data;
+      renderProductlist2();
+    });
+}
+
+
+
+function renderProductlist2() {
+  for (let index = 0; index < productList2.length; index++) {
+    const product2 = productList2[index];
+
+    const productItem2 = productTemp2.content.cloneNode(true);
+    const productImage2 = productItem2.getElementById("product-image2")
+    productImage2.src = product2.image;
+    productItem2.getElementById("product-title2").textContent = product2.child_type;
+    productImage2.addEventListener('click', function() {
+      console.log(1);
+      navigateToDetail2(product2.child_type)
+    })
+    productListElm2.appendChild(productItem2);
+  }
+}
+
+/////
+
+function navigateToDetail2(type) {
+  console.log(1);
+  window.location = `./detail.html?type=${type}`
+}
+
+
+
+
+
+getProductList2();
+
+
+
+
+
