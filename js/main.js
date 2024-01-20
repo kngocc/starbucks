88b989c69f585467aaa5b735117c8dce81c89c93
kngocc <kngocc66@gmail.com>
2024-01-20 22:56:51 +0700

    kkkk

--- js/main.js
+++ js/main.js
@@ -96,4 +96,61 @@ function transportHome() {
 const homeHtml = document.getElementById('symbol');
 homeHtml.addEventListener('click', transportHome)
 
+var interleaveOffset = 0.5;
+
+var swiperOptions = {
+  loop: true,
+  speed: 1000,
+  grabCursor: true,
+  watchSlidesProgress: true,
+  mousewheelControl: true,
+  keyboardControl: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    progress: function() {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        var slideProgress = swiper.slides[i].progress;
+        var innerOffset = swiper.width * interleaveOffset;
+        var innerTranslate = slideProgress * innerOffset;
+        swiper.slides[i].querySelector(".slide-inner").style.transform =
+          "translate3d(" + innerTranslate + "px, 0, 0)";
+      }      
+    },
+    touchStart: function() {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = "";
+      }
+    },
+    setTransition: function(speed) {
+      var swiper = this;
+      for (var i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = speed + "ms";
+        swiper.slides[i].querySelector(".slide-inner").style.transition =
+          speed + "ms";
+      }
+    }
+  }
+};
+
+var swiper = new swiper(".swiper-container", swiperOptions);
+
+// document.querySelector('[data-toggle]').addEventListener('click', function(){
+//   if (swiper.realIndex == 0) {
+//     swiper.slideTo(swiper.slides.length - 1);
+//   } else {
+//     swiper.slideTo(0);
+//   }
+// });
+
+// function logIndex () {
+//   requestAnimationFrame(logIndex);
+//   console.log(swiper.realIndex);
+// }
+// logIndex();
+
 
