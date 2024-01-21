fb812ca67c509683145f1fb4178b03a66cc34648
kngocc <kngocc66@gmail.com>
2024-01-21 14:53:17 +0700

    k

--- js/giftcard.js
+++ js/giftcard.js
@@ -11,3 +11,16 @@ function handleSignin() {
   
   const homeHtml = document.getElementById('symbol');
   homeHtml.addEventListener('click', transportHome)
+
+
+  $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
